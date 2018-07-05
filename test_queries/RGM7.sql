73a108d859306c0c6420250e4bdb940fc1063f6e
MikeeRead <40263903+MikeeRead@users.noreply.github.com>
2018-07-05 11:27:16 +0100

    initial query

---
+++ test_queries/RGM7.sql
@@ -0,0 +1,6 @@
+/* Q7: Ben Burningham wants to detect intrinsically faint nearby white dwarf stars through their high proper motions.
+Model this as a query on GPS for stars with reduced proper motion (from accurate proper motions) above some value as a function of colour. 
+
+Select reliable point sources with reliable proper motions and look at plot with Y-K and then the white dwarfs should be in a sparse, blue locus.*/
+
+/*No proper motions in DR8 */
