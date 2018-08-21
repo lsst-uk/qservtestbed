628d8f6e46f3b5494280f27c770327dfbc83702b
Darren White <djwhiteastro@gmail.com>
2018-08-21 16:19:03 +0100

    Moved DW2/3 QServ versions to corect directory, edited back to QSA verison. Still need to test!

--- wsa/DW2.sql
+++ wsa/DW2.sql
@@ -1,9 +1,9 @@
 /* 3. (query 047) - Select all variable objects within certain distance (e.g. 0.1 arcmin) of all known galaxies*/
 
-SELECT v.sourceID as vID, v.ra as vRA, v.decl as vDec, d.sourceID as dID, d.ra as dRA, d.decl as dDec, scisql_angSep(d.ra, d.decl, v.ra, v.decl) as angsep
-FROM UKIDSS_Qservp.dxsVariability AS v, UKIDSS_Qservp_dxsSource.dxsSource AS d
+SELECT v.sourceID as vID, v.ra as vRA, v.decl as vDec, d.sourceID as dID, d.ra as dRA, d.decl as dDec, dbo.fGreatCircleDist(d.ra, d.dec, v.ra, v.dec) as angsep
+FROM dxsVariability AS v, dxsSource AS d
 WHERE v.sourceID <> d.sourceID
  AND d.pGalaxy > 0.99
  AND v.jprobVar > 0.9
  AND v.ra > 0.0
- AND scisql_angSep(d.ra, d.decl, v.ra, v.decl) < 0.00167
+ AND dbo.fGreatCircleDist(d.ra, d.dec, v.ra, v.dec) < 0.1
