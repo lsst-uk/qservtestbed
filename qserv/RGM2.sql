e4817851f087967b5f198d7bd7c8036ba25e9b54
MikeeRead <40263903+MikeeRead@users.noreply.github.com>
2018-12-21 10:13:01 +0000

    Update RGM2.sql

--- qserv/RGM2.sql
+++ qserv/RGM2.sql
@@ -2,7 +2,7 @@
 /* Model this as a query for filenames for matching WSA images in different bands around positions of sources 
 selected according to a cut on ellipticity. 
 This is, obviously, very similar to Q1, but require images in Y, H and K for colour image.*/
-/*Higher ellipticities pulls out diffraction spikes, this mainly pulls out elliptical galaxies*/
+/*Higher ellipticities pulls out diffraction spikes, this mainly pulls out elliptical galaxies : 624 rows in set (15.54 sec) */
 
 Select s.ra as ra,s.decl as decl,y.fileName as yFile, l.yeNum as yExtNum from UKIDSSDR8_lasSource.lasSource as s, 
 UKIDSSDR8.lasMergeLog as l, UKIDSSDR8.Multiframe as y, UKIDSSDR8.Multiframe as h, UKIDSSDR8.Multiframe as k  
