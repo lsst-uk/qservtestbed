bfa56b6e0629e1d8aadef08f528bcd506915bbd7
MikeeRead <40263903+MikeeRead@users.noreply.github.com>
2018-12-21 10:21:50 +0000

    Update RGM5.sql

--- qserv/RGM5.sql
+++ qserv/RGM5.sql
@@ -4,4 +4,4 @@ Model this as extracting photometric and classification information from WSA for
 */
 select ra,decl,yDeblend,yAperMag3,yClass,yppErrBits, j_1Deblend,j_1AperMag3,j_1Class,j_1ppErrBits,hDeblend,hAperMag3,hClass,hppErrBits,
 kDeblend,kAperMag3,kClass,kppErrBits 
-from lasSource where qserv_areaspec_circle(181.6,-0.6,0.0333333);
+from UKIDSSDR8_lasSource.lasSource where qserv_areaspec_circle(181.6,-0.6,0.0333333);
