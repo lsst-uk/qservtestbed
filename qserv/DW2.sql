ced1cac5109a19b1ec6f4a766c9e37de677caec1
Darren White <djwhiteastro@gmail.com>
2019-02-05 12:04:12 +0000

    Tweak to previous commit - Added select blah *as name* to DW2, may fix bug.

--- qserv/DW2.sql
+++ qserv/DW2.sql
@@ -1,6 +1,6 @@
 /* 2. (query 007) - Select time series data for all objects in a given area of the sky, in a given photometric band (J, in our case) with a given variability index (>0.8) */
 
-SELECT s.sourceID as sID, v.jmeanMag as meanMag, m.mjdObs as MJD, d.aperMag3 as obsMag, d.aperMag3err as errMAg
+SELECT s.sourceID as ssID, v.jmeanMag as meanMag, m.mjdObs as MJD, d.aperMag3 as obsMag, d.aperMag3err as errMAg
 
 FROM UKIDSSDR8_dxsDetection.dxsDetection AS d, UKIDSSDR8_dxsSource.dxsSource AS s, UKIDSSDR8.dxsSourceXDetectionBestMatch AS x,UKIDSSDR8.Multiframe AS m, UKIDSSDR8.dxsVariability AS v
 
@@ -17,4 +17,4 @@ AND scisql_angSep(d.ra, d.decl,334.25,0.3) < 0.5
 
 AND scisql_angSep(d.ra, d.decl,s.ra,s.decl) < 0.1
 
-ORDER BY s.sourceID, m.mjdObs
+ORDER BY ssID, MJD
